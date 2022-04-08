986b5e0a5a41d222c0b0528f67c22a39d9eca8b8
vidalpc2010 <vidalpc2010@hotmail.com>
2022-04-08 16:27:45 -0500

    Dynamic Styles with CSS Modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,45 +1,45 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props) => (props.invalid ? "red" : "black")};
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.invalid ? "red" : "black")};
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-    background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+//     background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
 
-  // &.invalid input {
-  //   border-color: red;
-  //   background: rgb(253, 141, 141);
-  // }
+//   // &.invalid input {
+//   //   border-color: red;
+//   //   background: rgb(253, 141, 141);
+//   // }
 
-  // &.invalid label {
-  //   color: red;
-  // }
-`;
+//   // &.invalid label {
+//   //   color: red;
+//   // }
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -67,8 +67,9 @@ const CourseInput = (props) => {
     <form onSubmit={formSubmitHandler}>
       {/* Using JavaScript Template Strings ,  The best aproach is add CSS Class Dynamically  */}
       {/* <div className={`form-control ${!isValid ? "invalid" : ""}`}> */}
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
       {/* <FormControl className={!isValid && "invalid"}> */}
-      <FormControl invalid={!isValid}>
+      {/* <FormControl invalid={!isValid}> */}
         <label
         ///////////////  Inline Styles is not the best aproach
         // style={{ color: !isValid ? "red" : "black" }}
@@ -83,7 +84,7 @@ const CourseInput = (props) => {
           // }}
           onChange={goalInputChangeHandler}
         />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
